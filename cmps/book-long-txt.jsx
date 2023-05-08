b5693fe97b0dad63a5de170dd01145edacded3c4
Etai Levi <etai3000@gmail.com>
2023-05-08 10:26:07 +0300

    Improved the 'show more or less' text in the book-long-txt file

--- cmps/book-long-txt.jsx
+++ cmps/book-long-txt.jsx
@@ -2,11 +2,11 @@ const { useState } = React;
 
 export function LongTxt({ txt, length = 100 }) {
     const [isLong, setIsLong] = useState(false)
-    const [displayText, setDisplayText] = useState(txt.slice(0, length));
+    const [displayText, setDisplayText] = useState(txt.substring(0, length) + '...');
 
     function toggleTxtDisplay() {
         setIsLong(!isLong);
-        setDisplayText(isLong ? txt.slice(0, length) : txt);
+        setDisplayText(isLong ? txt.substring(0, length) + '...' : txt);
     }
 
     return (
